91fa309df0ee0d8d054d8dd76913c8be6633f613
dontregret9 <leduchoabatr12@gmail.com>
2023-09-06 01:04:26 +0700

    DB migrate

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,8 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-bundle exec rake db:setup
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rake db:seed
 
 bundle exec rails s -p 3000 -b 0.0.0.0
