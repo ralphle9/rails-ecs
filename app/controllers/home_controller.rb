114e942766646d332767440a399f8a6cb0c26cad
dontregret9 <leduchoabatr12@gmail.com>
2023-09-14 22:00:21 +0700

    Add logs for checking

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,24 @@
 class HomeController < ApplicationController
   def index
+    puts "I'm in index page"
     @message = "Dynamic"
 
     @posts = Post.all
   end
 
   def increment_async
-    ::IncrementCountWorker.perform_async(params[:post_id])
+    begin
+      puts "I'm in increment method"
+      puts '----------------------------------------------------------------'
+      ::IncrementCountWorker.perform_async(params[:post_id])
+      puts "I'm done call workers"
+      puts '----------------------------------------------------------------'
 
-    redirect_to root_path
+      redirect_to root_path
+    rescue Exception => e
+      puts 'We got this Sidekiq error'
+      p e   
+      puts '------------------------------------------------'
+    end
   end
 end
--- app/workers/increment_count_worker.rb
+++ app/workers/increment_count_worker.rb
@@ -2,10 +2,13 @@ class IncrementCountWorker
   include Sidekiq::Worker
 
   def perform(post_id)
+    puts "----------- #{post_id} -------"
     post = Post.find(post_id)
 
     post.likes_count ||= 0
     post.likes_count += 1
+    puts "-------- #{post.likes_count} ---------"
     post.save
+    puts "-------- #{post.errors.full_messages} ---------"
   end
 end
